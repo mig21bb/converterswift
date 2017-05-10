e64f4fd922fea33f2d5aa4cb4db895d543f0ce48
user128125 <user128125@77-240-12-9.rdns.melbourne.co.uk>
2017-05-10 10:24:04 -0700

    segué

---
+++ conversorswift/ViewController2.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController2.swift
+//  conversorswift
+//
+//  Created by user128125 on 5/10/17.
+//  Copyright © 2017 user128125. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //preparamos el código para el segue llamado calcular
+        if segue.identifier=="calcular"){
+            
+        }
+    }
+}
